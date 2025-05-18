c096b4f6e4f99550387e9fdc28f1fcb7b17cdc01
dmknpd <soupuabt@gmail.com>
2025-05-18 15:16:15 +0300

    small fixes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const subscriptionRoutes = require("./routes/subscriptionRoutes");
 const { startSchedulers } = require("./scheduler/scheduler");
 
 const app = express();
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
